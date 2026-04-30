1ce7f094dc1f36485fc1600c2989f82300773992
GGooby(or John) <victorstanciu69@gmail.com>
2026-04-30 17:54:46 +0300

    Add files via upload

---
+++ JS/lorem.js
@@ -0,0 +1,9 @@
+function darkmode() {
+	var element = document.body;
+   element.classList.toggle("dark-mode");
+} // yea.. i added dark mode.
+
+function why() {
+	alert("you can't question the lorem ipsum guys' page about lorem ipsum, dummy.")
+	console.log("he asked the lorem ipsum helper about the page.. WTF man he did his work why yuo becoume angy anti-loremguy?")
+}
